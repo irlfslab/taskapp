79c33cc50a48fad48e6a0b725944cf64d9ad4298
IL <53288945+irlfslab@users.noreply.github.com>
2024-09-28 19:20:14 -0400

    Add files via upload

---
+++ src/Clock1.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+
+function Clock1() {
+
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(newDate());
+        }, 1000);
+
+         {/* when unmount the component clear interval */}
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+    function formatTime() {
+        let hours = time.getHours();
+        const min = time.getMinutes();
+        const sec = time.getSeconds();
+        const meridiem = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+        return `${padTime(hours)}: ${padTime(min)}:${padTime(sec)} ${meridiem}`;
+    }
+
+    function padTime(num){
+        return (num > 10 ? "0" : "") + num;
+    }
+    
+    return (
+        <div className = "container1">
+            <div className="clock">
+                <span>00:00:00</span>
+            </div>
+        </div>
+    );
+}
+
+export default Clock1;
